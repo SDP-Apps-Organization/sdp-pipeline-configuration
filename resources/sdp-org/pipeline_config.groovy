5db4884be61ade3239fdf83fad73ff9f2d6f96ae
Alec Keller <keller_alec@bah.com>
2018-12-19 14:19:03 -0500

    added app envs

--- resources/sdp-org/pipeline_config.groovy
+++ resources/sdp-org/pipeline_config.groovy
@@ -6,4 +6,33 @@ libraries{
   github_enterprise
   docker
   marketplace
+  //ucp
+}
+
+keywords{
+  override = true
+  master  =  /^[Mm]aster$/
+  develop =  /^[Dd]evelop(ment|er|)$/
+  hotfix  =  /^[Hh]ot[Ff]ix-/
+  release =  /^[Rr]elease-(\d+.)*\d$/
+}
+
+application_environments{
+  override = true
+  dev{
+     short_name = "dev"
+     long_name = "Development"
+  }
+  test{
+     short_name = "test"
+     long_name = "Test"
+  }
+  staging{
+     short_name = "stage"
+     long_name = "Staging"
+  }
+  prod{
+     short_name = "prod"
+     long_name = "Production"
+  }
 }
